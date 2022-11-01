55f6119ca7c324a8e3971a11da48e2306af5519f
NikolasZM <101744161+NikolasZM@users.noreply.github.com>
2022-11-01 17:50:20 -0500

    Add files via upload

--- pokemon.cpp
+++ pokemon.cpp
@@ -32,8 +32,9 @@ public:
         defensa = 0;
         tipo1="No";
         tipo2="No";
+        color="";
     }
-    void setInfo(string especiePoke,int nivelPok, int vidaPok,int ataquePok, int defensaPok, string tipo1Pok, string tipo2Pok, string colorPok) {
+    void setInfo(string especiePoke,int nivelPok, int vidaPok,int ataquePok, int defensaPok, string tipo1Pok, string tipo2Pok,string colorPok) {
         especie=especiePoke;
         nivel=nivelPok;
         vida=vidaPok;
@@ -41,15 +42,23 @@ public:
         defensa=defensaPok;
         tipo1=tipo1Pok;
         tipo2=tipo2Pok;
+        color=colorPok;
     }
     void getInfo() {
         cout<<"======================\nEspecie: "<<especie<<"\nTipo1: "<<tipo1<<"\nTipo2: "<<tipo2<<"\nVida: "<<vida<<"\nAtaque: "<<ataque<<"\nDefensa: "<<defensa<<"\n======================\n";
+    }
+    void getSprite() {
+        string pintar = "\x1b"+color;
+        cout <<pintar<<"███\t\t"<<DF<<especie<<"\n";
+        cout <<pintar<<"███\t\t"<<DF<<"Vida: "<<vida<<"\n";
+        cout <<pintar<<"███\n"<<DF<<"\n";
     }
 private:
     string especie;
     int nivel;
     string tipo1;
     string tipo2;
+    string color;
     int vida;
     int ataque;
     int defensa;
@@ -63,10 +72,9 @@ void crearInicial(string archivoData) {
   Pokemon inicial[3];
   getline(archivo, linea);
   int i=0;
-  system("cls");
   while (getline(archivo, linea)) {         //Crear iniciales
     stringstream stream(linea);
-    string id,especie,tipo1,tipo2,vida,ataque,defensa;
+    string id,especie,tipo1,tipo2,vida,ataque,defensa,color;
     getline(stream,id,del);
     if (id=="1"||id=="4"||id=="7") {
         getline(stream,especie,del);
@@ -75,10 +83,12 @@ void crearInicial(string archivoData) {
         getline(stream,vida,del);
         getline(stream,ataque,del);
         getline(stream,defensa,del);
+        getline(stream,color);
         int idInt=stoi(id);
         int vidaInt=stoi(vida),ataqueI=stoi(ataque),defensaI=stoi(defensa);
-        inicial[i].setInfo(especie,5,vidaInt,ataqueI,defensaI,tipo1,tipo2);
+        inicial[i].setInfo(especie,5,vidaInt,ataqueI,defensaI,tipo1,tipo2,color);
         inicial[i].getInfo();
+        inicial[i].getSprite();
         i++;
     }else {
         continue;
@@ -146,6 +156,6 @@ system("cls");
 cout<<"Es hora de que escojas a tu primer pokemon...\n";
 string pokemonArchivo = "E:/NewLabCompiler/ProjectTest_PathCompiler/src/Project_0012/DatosC.csv";
 crearInicial(pokemonArchivo);
-cout<<"Estos son nuestros pokemons iniciales:\n";
+cout<<"Cual eliges?\n";
 
 }
